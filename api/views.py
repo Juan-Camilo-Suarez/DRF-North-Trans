972ae29a5a4436d8f16a857d279ffafdf5b58a2b
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-12-04 22:52:36 +0300

    config urls and example of view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def main_api(request):
+    return JsonResponse({
+        'status': 'ok'
+    })
