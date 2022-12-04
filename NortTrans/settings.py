5487a87a5df9695a3b49497c8a550c7396e1d004
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-12-04 22:34:29 +0300

    correction on structur in project

--- NortTrans/settings.py
+++ NortTrans/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%c=$xtekl7zwc*%1zp6jb)a=-q77%6r8=0x)bdml8e%qz+vfup'
+SECRET_KEY = 'django-insecure-$@=8wkz9%n_rd9mpwf31w9^u!^lvvtlkld56az_a5_ecn%$8@o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
