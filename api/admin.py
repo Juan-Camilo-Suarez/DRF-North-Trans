e453191a253d9a7edf34e71fd491f784ab9fe8f0
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-12-11 20:51:38 +0300

    + model application register

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from api.models import ApplicationRegister
+
 # Register your models here.
+admin.site.register(ApplicationRegister)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class ApplicationRegister(models.Model):
+    class TypeUser(models.TextChoices):
+        client = "client", "client"
+        driver = "driver", "driver"
+
+    name = models.CharField(max_length=120, blank=False, verbose_name="Name")
+    city = models.CharField(max_length=120, blank=False, verbose_name="City")
+    email = models.EmailField(max_length=120, blank=False, verbose_name="Email")
+    phone = models.CharField(max_length=120, blank=False, verbose_name="Phone")
+    user_position = models.CharField(
+        max_length=15,
+        choices=TypeUser.choices,
+        verbose_name="Type User",
+    )
+
+    class Meta:
+        verbose_name = "Application Register"
+        verbose_name_plural = "Applications Register"
